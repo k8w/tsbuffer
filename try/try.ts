160428d9e2d59186ed44fce88534bea58c9ec6b7
k8w <twoeo@qq.com>
2019-06-05 20:35:40 +0800

    try encode interface

--- try/try.ts
+++ try/try.ts
@@ -7,11 +7,79 @@ let tsb = new TSBuffer({
         },
         a2: {
             type: 'String'
+        },
+        a3: {
+            type: 'Interface',
+            properties: [{
+                id: 0,
+                name: 'e1',
+                type: { type: 'String' }
+            }, {
+                id: 1,
+                name: 'e2',
+                type: { type: 'Number' }
+            }]
+        },
+        a4: {
+            type: 'Interface',
+            extends: [{
+                id: 0,
+                type: {
+                    type: 'Reference',
+                    path: 'a',
+                    targetName: 'a3'
+                }
+            }],
+            properties: [{
+                id: 0,
+                name: 'value1',
+                type: { type: 'String' }
+            }, {
+                id: 1,
+                name: 'value2',
+                type: {
+                    type: 'Reference',
+                    path: 'a',
+                    targetName: 'a3'
+                }
+            }],
+            indexSignature: {
+                keyType: 'String',
+                type: { type: 'Any' }
+            }
         }
     }
 });
 
 let res = tsb.encode(12345, 'a', 'a1');
 let res1 = tsb.encode('哈哈哈abc', 'a', 'a2');
+let v2 = {
+    is1: 'aaa',
+    is2: [1, 2, 3],
+    e1: 'sss',
+    e2: 222,
+    value2: {
+        e2: 123456,
+        e1: 'aaa'
+    },
+    value1: 'Fuck you all'
+
+};
+let res2 = tsb.encode(v2, 'a', 'a4');
 console.log(res);
 console.log(res1);
+console.log(res2);
+
+console.log('压缩率对比', res2.length, JSON.stringify(v2).length);
+
+console.time('TSBuffer encode');
+for (let i = 0; i < 1000000; ++i){
+    let res = tsb.encode(v2, 'a', 'a4');
+}
+console.timeEnd('TSBuffer encode');
+
+console.time('JSON stringify');
+for (let i = 0; i < 1000000; ++i) {
+    let res = JSON.stringify(v2);
+}
+console.timeEnd('JSON stringify');
