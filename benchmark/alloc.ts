29149dabe9d8284a65b74156c8d1c67a0fda52a2
k8w <me@k8w.io>
2019-11-27 20:49:03 +0800

    utf8 test & benchmark

--- benchmark/alloc.ts
+++ benchmark/alloc.ts
@@ -1,5 +1,7 @@
 import 'k8w-extend-native';
 
+console.log('写Buffer性能测试')
+
 function test(str: string, fun: Function) {
     let times = 1;
     let startTime = process.uptime();
@@ -8,12 +10,18 @@ function test(str: string, fun: Function) {
     }
     console.log(str, (process.uptime() - startTime) * 1000 / times + 'ms');
 }
+
+let srcBuf1M!: Uint8Array;
 test('一次写100万', () => {
     let buf = new ArrayBuffer(1000 * 1000);
-    let arr = new Uint8Array(buf);
+    srcBuf1M = new Uint8Array(buf);
     for (let i = 0; i < buf.byteLength; ++i) {
-        arr[i] = i & 255;
+        srcBuf1M[i] = i & 255;
     }
+});
+
+test('一次复制100万', () => {
+    new Uint8Array(1000 * 1000).set(srcBuf1M);
 });
 
 let src = Array.from({ length: 1000 }, (v, i) => i & 255);
@@ -26,15 +34,23 @@ test('1000次写1000到整Buffer', () => {
 });
 
 test('1000次写1000到独立Buffer', () => {
-    let arr = [];
-    for (let i = 0; i < 1000; ++i){
-        arr.push(new Uint8Array(src))
+    for (let i = 0; i < 1000; ++i) {
+        new Uint8Array(src)
     }
 });
+
+let srcBuf = new Uint8Array(src);
+test('1000次分配并复制长1000的Buffer', () => {
+    for (let i = 0; i < 1000; ++i) {
+        new Uint8Array(1000).set(srcBuf)
+    }
+})
 
 test('分配1000次Buffer', () => {
     let arr = [];
     for (let i = 0; i < 1000; ++i) {
-        arr.push(new Uint8Array(1000))
+        let buf = new Uint8Array(1000);
+        buf[0] = 123;
+        arr.push(buf)
     }
 })
