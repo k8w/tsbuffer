fc3530ea72049cc3951164f09948c6ba54dd516b
k8w <me@k8w.io>
2021-03-26 11:45:05 +0800

    1.4.7 mjs es6

--- rollup.config.js
+++ rollup.config.js
@@ -8,7 +8,33 @@ export default [
             format: 'cjs',
             file: './dist/index.cjs',
             banner: require('./scripts/copyright')
-        }, {
+        }],
+        plugins: [
+            typescript({
+                tsconfigOverride: {
+                    compilerOptions: {
+                        target: 'es5',
+                        declaration: false,
+                        declarationMap: false,
+                        module: "esnext"
+                    }
+                }
+            }),
+            terser({
+                mangle: {
+                    properties: {
+                        regex: /^_/
+                    }
+                },
+                format: {
+                    comments: /^!/
+                }
+            })
+        ]
+    },
+    {
+        input: './src/index.ts',
+        output: [{
             format: 'es',
             file: './dist/index.mjs',
             banner: require('./scripts/copyright')
@@ -17,6 +43,7 @@ export default [
             typescript({
                 tsconfigOverride: {
                     compilerOptions: {
+                        target: 'es6',
                         declaration: false,
                         declarationMap: false,
                         module: "esnext"
