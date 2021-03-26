fc1b5b9758bdf354c018c30d53ec0ace87a45590
k8w <me@k8w.io>
2021-03-26 10:50:13 +0800

    coverage

--- try/varint.ts
+++
@@ -1,8 +0,0 @@
-import { Varint64 } from '../src/models/Varint64';
-
-let v = Varint64.from(-1);
-console.log((v as any)._uint32s);
-v.zzEncode();
-console.log((v as any)._uint32s);
-v.zzDecode();
-console.log((v as any)._uint32s);
