f78ed2db68c5924eb7b7dae5fc3c25880fd2308d
King Wang <me@k8w.io>
2021-03-28 10:37:26 +0800

    update lic

--- scripts/copyright.js
+++ scripts/copyright.js
@@ -1,7 +1,7 @@
 module.exports = `/*!
  * TSBuffer v${require('../package.json').version}
  * -----------------------------------------
- * Apache 2.0 LICENSE
+ * LGPL 3.0 LICENSE
  * KingWorks (C) Copyright ${new Date().getFullYear()}
  * https://github.com/k8w/tsbuffer
  */`
