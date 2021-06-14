3ac780c6a3971f84da97f3c88676750bb3cb76ba
King Wang <me@k8w.io>
2021-06-15 00:53:36 +0800

    MIT

--- scripts/copyright.js
+++ scripts/copyright.js
@@ -1,7 +1,7 @@
 module.exports = `/*!
  * TSBuffer v${require('../package.json').version}
  * -----------------------------------------
- * LGPL 3.0 LICENSE
+ * MIT LICENSE
  * KingWorks (C) Copyright ${new Date().getFullYear()}
  * https://github.com/k8w/tsbuffer
  */`
