33cd5977a137aec586789096977f9455f42bf07d
k8w <me@k8w.cn>
2023-02-17 22:23:24 +0800

    rm unused files

--- packages/proto-generator/scripts/copyright.js
+++
@@ -1,7 +0,0 @@
-module.exports = `/*!
- * TSBuffer Proto Generator v${require('../package.json').version}
- * -----------------------------------------
- * MIT LICENSE
- * KingWorks (C) Copyright ${new Date().getFullYear()}
- * https://github.com/k8w/tsbuffer
- */`
